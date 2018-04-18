1db37f0758518c764e8c83b44031c2503d211042
KimTaeHyeong17 <uuzaza@naver.com>
2018-04-18 21:26:56 +0900

    nothing

---
+++ week2/assignment/week2_KimTaeHyeong/Mondiraan/app/src/main/java/com/example/kimtaehyeong/week1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kimtaehyeong.week1;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);  //R android project keyword saved.
+    }
+}
