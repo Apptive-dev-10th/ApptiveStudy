bf3139f694ab6c72d9d2a5e14ddc93db9cfb0f0b
jm1224k <jm1224k@gmail.com>
2018-04-27 18:50:50 +0900

    죄송해요..ㅠㅠ

---
+++ week3/assignment/week3_KimJeongMee/app/src/main/java/e/jeong_mi/week3/MainActivity.java
@@ -0,0 +1,74 @@
+package e.jeong_mi.week3;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager vp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vp = (ViewPager)findViewById(R.id.vp);
+        Button btn_first = (Button)findViewById(R.id.btn_first);
+        Button btn_second = (Button)findViewById(R.id.btn_second);
+        Button btn_third = (Button)findViewById(R.id.btn_third);
+        Button btn_fourth = (Button)findViewById(R.id.btn_fourth);
+
+        vp.setAdapter(new pagerAdapter(getSupportFragmentManager()));
+        vp.setCurrentItem(0);
+
+        btn_first.setOnClickListener(movePageListener);
+        btn_first.setTag(0);
+        btn_second.setOnClickListener(movePageListener);
+        btn_second.setTag(1);
+        btn_third.setOnClickListener(movePageListener);
+        btn_third.setTag(2);
+        btn_fourth.setOnClickListener(movePageListener);
+        btn_third.setTag(3);
+    }
+
+    View.OnClickListener movePageListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v)
+        {
+            int tag = (int) v.getTag();
+            vp.setCurrentItem(tag);
+        }
+    };
+
+    private class pagerAdapter extends FragmentStatePagerAdapter
+    {
+        public pagerAdapter(android.support.v4.app.FragmentManager fm)
+        {
+            super(fm);
+        }
+        @Override
+        public android.support.v4.app.Fragment getItem(int position)
+        {
+            switch(position)
+            {
+                case 0:
+                    return new firstfragment();
+                case 1:
+                    return new secondfragment();
+                case 2:
+                    return new thirdfragment();
+                case 3:
+                    return new fourthfragment();
+                default:
+                    return null;
+            }
+        }
+        @Override
+        public int getCount()
+        {
+            return 4;
+        }
+    }
+}
---
+++ week3/assignment/week3_KimJeongMee/app/src/main/java/e/jeong_mi/week3/thirdfragment.java
@@ -0,0 +1,28 @@
+package e.jeong_mi.week3;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class thirdfragment extends Fragment
+{
+    public thirdfragment()
+    {
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_first, container, false);
+        return layout;
+    }
+}
