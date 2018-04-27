12eced2e9aa6be4d2a27b5bc7a26f24a01f4ced8
KwangSuCHO <cks5654@naver.com>
2018-04-27 18:50:11 +0900

    화이또

---
+++ week3/assignment/Week3_KwangSu/app/src/main/java/com/example/kakaotalk2/FragmentThree.java
@@ -0,0 +1,32 @@
+package com.example.kakaotalk2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Win7 on 2018-04-27.
+ */
+
+public class FragmentThree extends Fragment
+{
+    public FragmentThree()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        View layout = inflater.inflate(R.layout.fragment_3, container, false);
+        return layout;
+    }
+}
