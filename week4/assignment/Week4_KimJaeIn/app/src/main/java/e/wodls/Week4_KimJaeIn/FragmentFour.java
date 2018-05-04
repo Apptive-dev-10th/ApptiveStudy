a2de351aa8fc2a75ec4c3a769dfe6e8ad8541a95
Belie21 <wodls7209@naver.com>
2018-05-04 22:24:14 +0900

    저희 위해서 정말 고생 많으신데, 저번주에 이어서 정말 죄송합니다

---
+++ week4/assignment/Week4_KimJaeIn/app/src/main/java/e/wodls/Week4_KimJaeIn/FragmentFour.java
@@ -0,0 +1,16 @@
+package e.wodls.Week4_KimJaeIn;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFour extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tab_four, container, false);
+
+        return view;
+    }
+}
---
+++ week4/assignment/Week4_KimJaeIn/app/src/main/java/e/wodls/Week4_KimJaeIn/IntentActivity.java
@@ -0,0 +1,12 @@
+package e.wodls.Week4_KimJaeIn;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class IntentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}
