2eaef72e92846b8630816fe598e9f099cce650d1
jm1224k <jm1224k@gmail.com>
2018-05-09 02:57:06 +0900

    week5-KimJeongMee

---
+++ week5/assignment/week05_KimJeongMee/app/src/main/java/jeongmee/amanda/FirstFragment.java
@@ -0,0 +1,27 @@
+package jeongmee.amanda;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class FirstFragment extends Fragment
+{
+    public FirstFragment()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_first, container, false);
+        return layout;
+    }
+}
---
+++ week5/assignment/week05_KimJeongMee/app/src/main/java/jeongmee/amanda/ThirdFragment.java
@@ -0,0 +1,27 @@
+package jeongmee.amanda;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class ThirdFragment extends Fragment
+{
+    public ThirdFragment()
+    {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fragment_third, container, false);
+        return layout;
+    }
+}
