45ed4f956825745f66afa2365102635356eb7fc0
2marking <cjswoakzld@naver.com>
2018-05-12 06:30:51 +0900

    Week5_LeeGyoWon

---
+++ week5/assignment/Week5_LeeGyoWon/app/src/main/java/com/example/leegyowon/week5_leegyowon/Fragment2.java
@@ -0,0 +1,15 @@
+package com.example.leegyowon.week5_leegyowon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_t2, container, false);
+        return view;
+    }
+}
